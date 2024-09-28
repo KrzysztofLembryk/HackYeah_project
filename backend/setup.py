b4c2133b36f811808a43364a7c0392426889a550
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-09-28 11:45:37 +0200

    python project setup, setup.py etc

---
+++ backend/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your project's dependencies here
+        # e.g., 'requests >= 2.19.1',
+    ],
+    # Add all other necessary information here
+)
